ee64ca34bd2d2c7069491937b119246ee3b4321e
jameschun0985 <jameschun0985@gmail.com>
2025-04-11 14:31:50 +0800

    Update .gitignore and next.config.js for production settings; clean up package.json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/Base64EncodeDecode' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? './' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Base64EncodeDecode' : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
